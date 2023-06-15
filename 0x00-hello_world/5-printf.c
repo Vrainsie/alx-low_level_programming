96c5975c445d141352d0ee0d463ed7494f800bbe
Vrainsie <vickessyrainson@gmail.com>
2023-06-15 07:48:27 -0700

    printf() for stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Return: 0  on success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
