057058f768c26e401e85966970d52c0a5de9d7e5
Vrainsie <vickessyrainson@gmail.com>
2023-06-15 07:33:53 -0700

    Using put() for stdout

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * Main is entry
- * Return 0 on success
+ * main - is an  entry
+ *
+ * Return: 0 on success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
