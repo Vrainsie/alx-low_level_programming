5bfbace463309f2c9ca86fca7ebc6aeb485c8d93
Vrainsie <vickessyrainson@gmail.com>
2023-06-17 02:52:21 -0700

    Combined Printing

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,8 +17,8 @@ int main(void)
 			putchar(num1 / 10 + '0');
 			putchar(num1 % 10 + '0');
 			putchar(' ');
-			putchar(num1 % 10 + '0');
-			putchar(num1 % 10 + '0');
+			putchar(num2 % 10 + '0');
+			putchar(num2 % 10 + '0');
 
 			if (num1 != 99 || num2 != 99)
 			{
