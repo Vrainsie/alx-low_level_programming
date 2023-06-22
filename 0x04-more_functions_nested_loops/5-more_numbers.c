074ae89f0c671b88711444a465a285c75620aae3
Vrainsie <vickessyrainson@gmail.com>
2023-06-22 02:18:42 -0700

    Multi nos printer

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers on new lines
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int rows = 0;
+
+	while (rows <= 10)
+	{
+		int x = 0;
+
+        	while (x <= 14)
+                {
+	        	if (x > 9)
+	         	{
+		        	_putchar((x / 10) + '0');
+		        	_putchar((x % 10) + '0');
+	        	}
+	        	else
+	        	{
+		        	_putchar(x + '0');
+	        	}
+
+	        	x++;
+                }
+		_putchar('\n');
+		rows++;
+	}
+}
