76e69bad486732704f95541abf9491b043f9d839
Vrainsie <vickessyrainson@gmail.com>
2023-06-26 07:13:44 -0700

    String counter

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -checks length of string
+ * @s: string to check
+ * Return: String length  on success
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
