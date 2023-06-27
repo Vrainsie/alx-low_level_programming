1bef476f408d7454daf8a67435a677db2e9ceed2
Vrainsie <vickessyrainson@gmail.com>
2023-06-27 01:20:41 -0700

    Prints second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints last half
+ * @str: param
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int x;
+	int y;
+	int t;
+	int z = 0;
+
+	while (str[z] != '\0')
+	{
+		z++;
+	}
+
+	if (z % 10 == 0)
+	{
+		x = (z / 2);
+	}
+	else
+	{
+		x = (z - 1) / 2;
+	}
+
+	for (y = 0; y <= x; y++)
+	{
+		t = x + y;
+		_putchar(str[t]);
+	}
+
+	_putchar('\n');
+}
