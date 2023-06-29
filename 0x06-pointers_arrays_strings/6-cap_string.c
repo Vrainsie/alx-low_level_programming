97846b1dc9933458093dcd258c346594894fb1df
Vrainsie <vickessyrainson@gmail.com>
2023-06-29 12:11:44 -0700

    Capitalizes each first lettter

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * cap_string - Capitalize all 
+  * @s: param
+  *
+  * Return: converted string
+  */
+char *cap_string(char *s)
+{
+	int a = 0, b;
+	int c = 13;
+	char chr[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (b < c)
+		{
+			if ((a == 0 || s[a - 1] == chr[b]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+
+		       b++;
+		}
+
+		a++;
+	}
+
+	return (s);
+}
