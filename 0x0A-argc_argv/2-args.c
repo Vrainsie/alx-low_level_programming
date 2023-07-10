d9d331295d8b66ecbe14cd9db73ceba294039bd3
Vrainsie <vickessyrainson@gmail.com>
2023-07-10 01:19:34 -0700

    Print every vector passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all argv passed
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 on scs
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	while (i < argc)
+	{
+		printf("%s \n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
