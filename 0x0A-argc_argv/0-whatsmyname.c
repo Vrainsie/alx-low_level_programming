7ef36be05a252acedb9b8a22c836bbcf283d56b3
Vrainsie <vickessyrainson@gmail.com>
2023-07-10 00:17:51 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 on scs
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s \n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Argument Count and argument Vectors
