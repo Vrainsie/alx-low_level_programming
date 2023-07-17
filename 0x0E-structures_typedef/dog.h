605b45ef904af01f308176f514116707728d3097
Vrainsie <vickessyrainson@gmail.com>
2023-07-17 03:44:53 -0700

    Start on structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Dog descriptor
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ *
+ * Description: Dog x-tics
+ */
+
 struct dog
 {
 	char *name;
